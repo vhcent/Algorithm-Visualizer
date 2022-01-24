a6b40a259676ffc1bf6276e87d76e619f503b271
Xiye <84062739+Xiyeh@users.noreply.github.com>
2022-01-23 17:44:57 -0800

    Empty Grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+//import { render } from "@testing-library/react";
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//components
+import Node from "./components/Node";
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nodes: [],
+        };
+    }
+
+    componentDidMount() {
+        const nodes = [];
+        for (let row = 0; row < 15; row++) {
+            const currentRow = [];
+            for (let column = 0; column < 50; column++) {
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({ nodes: nodes });
+    }
+
+    render() {
+        const grid = this.state.nodes;
+
+        return (
+            <div className="grid">
+                {grid.map((row, rowIndex) => {
+                    return (
+                        <div class="row">
+                            {row.map((node, nodeIndex) => (
+                                <Node></Node>
+                            ))}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 }
-
-export default App;
