ef7638e1d21c2cadb855b20cd249a67f2dd204b8
Aroop Biswal <aroopbiswal@gmail.com>
2022-01-28 20:33:13 -0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+//import { render } from "@testing-library/react";
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//components
+import Node from "./components/Node";
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nodes: [],
+        };
+    }
+
+    componentDidMount() {
+        const nodes = [];
+        for (let row = 0; row < 18; row++) {
+            const currentRow = [];
+            for (let column = 0; column < 50; column++) {
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({ nodes: nodes });
+    }
+
+    render() {
+        const grid = this.state.nodes;
+
+        return (
+                <div className="header">
+                    <div className="title">
+                        Pathfinding Algorithm Visualizer
+                    </div>
+                    <div className="dropdown-container">
+                    <div className="dropdown">
+                        Algorithms
+                        <div className="dropdown-content">
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                Dijkstra's
+                            </button>
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                A*
+                            </button>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        Terrain
+                        <div className="dropdown-content">
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                Recursive Maze
+                            </button>
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                A*
+                            </button>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        Speed
+                        <div className="dropdown-content">
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                Slow
+                            </button>
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                Medium
+                            </button>
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                Fast
+                            </button>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        Reset
+                        <div className="dropdown-content">
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                Reset Board
+                            </button>
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                Clear Walls
+                            </button>
+                            <button
+                                className="dropdown-button"
+                                onClick={() => {}}
+                            >
+                                Clear Path
+                            </button>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="spacing">
+
+                    </div>
+                    <div className="grid">
+                        {grid.map((row, rowIndex) => {
+                            return (
+                                <div class="row">
+                                    {row.map((node, nodeIndex) => (
+                                        <Node></Node>
+                                    ))}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+        );
+    }
 }
-
-export default App;
